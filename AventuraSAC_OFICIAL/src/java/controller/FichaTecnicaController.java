1b50a5b4809b2cffe0c27365b1add0f01e3ba13c
Sabrina Bv <Sabrina Bv@192.168.1.6>
2019-11-29 20:22:46 -0500

    Acaba mis controladoras pero no ejecutan las vistas, ayudaaaa, te fijas :,v

--- AventuraSAC_OFICIAL/src/java/controller/FichaTecnicaController.java
+++ AventuraSAC_OFICIAL/src/java/controller/FichaTecnicaController.java
@@ -7,6 +7,7 @@ import javax.persistence.Persistence;
 import model.controllers.FichatecnicaJpaController;
 import model.controllers.TipotelaJpaController;
 import model.entities.Fichatecnica;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
-
+@Controller
 public class FichaTecnicaController  {
     
         private EntityManager em;
